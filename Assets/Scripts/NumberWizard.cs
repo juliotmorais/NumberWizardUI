aaa4cf693bb45ab6ef60b16e2198633762086c1b
juliotmorais <juliotmorais@gmail.com>
2020-07-20 22:00:08 +0900

    beta version of the game

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class NumberWizard : MonoBehaviour
+{
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessedNumber;
+    [SerializeField] Button higher;
+    [SerializeField] Button lower;
+    int guess;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess+1;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess-1;
+        NextGuess();
+    }
+
+    void StartGame()
+    {
+
+        NextGuess();
+
+
+        higher.onClick.AddListener(OnPressHigher);
+        lower.onClick.AddListener(OnPressLower);
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max+1);
+        guessedNumber.text = guess.ToString();
+    }
+
+
+}
